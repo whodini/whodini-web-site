ba79a601c02e83272fc5509ead1fec10d139f425
Tim Towles <timtowles@Tims-MacBook-Air-2.local>
2013-08-01 16:00:11 -0700

    Check for environment before setting email param

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,12 +3,10 @@ class WelcomeController < ApplicationController
   end
 
   def redirect_to_whodini_app
-    Rails.env.development?
-      new_url = "http://localhost:3000/" << '?email='+params[:user][:address]
-    Rails.env.test?
-      new_url = "http://ec2-54-215-119-173.us-west-1.compute.amazonaws.com/" << '?email='+params[:user][:address]
-    Rails.env.production?
-      #TODO: add production endpoint
+    @params = '?email='+params[:user][:address]
+    new_url = "http://localhost:3000/" << @params if Rails.env.development? 
+    new_url = "http://ec2-54-215-119-173.us-west-1.compute.amazonaws.com/" << @params if Rails.env.test?
+    new_url = 'TODO: add production endpoint' << @params if Rails.env.production?
     redirect_to new_url
   end
 end
