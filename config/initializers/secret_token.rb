c8ea7aeb9af0c2a35e7d2c9bf19476a693378cbb
Tim Towles <timtowles@Tims-MacBook-Air-2.local>
2013-07-31 17:15:23 -0700

    Bootstrap the corporate site with the whodini_main engine

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhodiniWebSite::Application.config.secret_token = 'de1e5d499d592b61760458b1638e2d2460f3bfe98ae4f1e6783b7adc49b5a7d15cbf95a97521eb15f02e9780da969554b005087f95dbffb280ab58402284808b'
