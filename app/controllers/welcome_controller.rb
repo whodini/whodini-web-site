a41c4def059727054f878de182ca7d7b204d8273
Tim Towles <timtowles@Tims-MacBook-Air-2.local>
2013-08-01 15:50:14 -0700

    Add local and test urls for the redirect

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,11 +3,12 @@ class WelcomeController < ApplicationController
   end
 
   def redirect_to_whodini_app
-    # subdomain = request.subdomain
-    # if subdomain == 'secure'
-    # params = request.original_fullpath.split("?").second
-    new_url = "http://localhost:10524/" << '?email='+params[:user][:address]
+    Rails.env.development?
+      new_url = "http://localhost:3000/" << '?email='+params[:user][:address]
+    Rails.env.test?
+      new_url = "http://ec2-54-215-119-173.us-west-1.compute.amazonaws.com/" << '?email='+params[:user][:address]
+    Rails.env.production?
+      #TODO: add production endpoint
     redirect_to new_url
-    # end
   end
 end
