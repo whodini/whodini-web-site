faa2a6c32a52d49c2b21236e36595de73eba7deb
Sumit Vij <sumitvij11@gmail.com>
2013-08-05 15:32:55 -0700

    Added other deployment commands

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,7 +5,7 @@ class WelcomeController < ApplicationController
   def redirect_to_whodini_app
     @params = '?email='+params[:user][:address]
     new_url = "http://localhost:3000/" << @params if Rails.env.development? 
-    new_url = "http://ec2-54-215-119-173.us-west-1.compute.amazonaws.com/" << @params if Rails.env.test?
+    new_url = "http://ec2-54-215-119-173.us-west-1.compute.amazonaws.com/" << @params if Rails.env.aws_dev?
     new_url = 'TODO: add production endpoint' << @params if Rails.env.production?
     redirect_to new_url
   end
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,7 @@
 set :application, "Whodini-corporate"
-set :repository,  "set your repository location here"
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
-
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
 
@@ -23,3 +16,84 @@ role :db,  "your slave db-server here"
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
 # end
+#
+set :deploy_to, "/opt/whodini"
+ssh_options[:keys] = [File.join(ENV["HOME"],".ssh","devinlogy.pem")]
+require 'cap_git_tools/tasks'
+#Add groups and server info from the AWS
+require 'capawsext/tasks'
+require 'capawsext/whoec2helper'
+#All commands are executing in the context of this region only.
+set :aws_region, 'us-west-1'
+add_groups(fetch(:aws_region))
+
+#http://gembundler.com/v1.3/deploying.html
+require 'bundler/capistrano'
+
+#https://help.github.com/articles/deploying-with-capistrano
+set :user, "root"
+default_run_options[:pty] = true
+set :scm, "git"
+set :branch, "master"
+set :deploy_via, :remote_cache
+set :repository,  "https://b869b554d0672dd43377a32d9ec89ada0ce1da6b:x-oauth-basic@github.com/whodini/whodini-web-site.git"
+
+set :rvm_ruby_string, "2.0.0"               # use the same ruby as used locally for deployment
+set :rvm_autolibs_flag, :fail        # more info: rvm help autolibs
+
+before 'deploy:setup', 'rvm:install_rvm'   # install RVM
+before 'deploy:setup', 'rvm:install_ruby'  # install Ruby and create gemset, OR:
+before 'deploy:setup', 'rvm:create_gemset' # only create gemset
+
+require "rvm/capistrano"
+set :ec2_helper, Ec2Helper.new(fetch(:aws_region))
+
+# if you want to clean up old releases on each deploy uncomment this:
+after "deploy:update", "deploy:dbcreate"
+after "deploy:restart", "deploy:cleanup"
+
+namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+	
+	desc "Start the thin process"
+	task :start, :roles => :'app' do
+		run_thin_cmd("start")
+	end
+
+	desc "Stop the thin process"
+	task :stop, :roles => :'app' do
+		run_thin_cmd("stop")
+	end
+
+	desc "Restart the thin process"
+	task :restart, :roles => :'app'  do
+		run_thin_cmd("restart")
+	end
+
+  desc "Create dummy db"
+  task :dbcreate, :roles => :'app'  do
+    run_remote_rake("db:create")
+  end
+
+	def run_thin_cmd(command)
+		run wrapin_bundle_command("thin #{command} -p 80 -d -e #{fetch(:env)}")
+	end
+
+end
+
+ #Define the helpers method
+def wrapin_bundle_command(command)
+  "cd #{fetch(:current_path)} && RAILS_ENV=\"#{fetch(:env)}\" bundle exec #{command}"
+end
+def run_remote_rake(rake_cmd)
+	rake_args = ENV['RAKE_ARGS'].to_s.split(',')
+	cmd = "cd #{fetch(:current_path)} && #{fetch(:rake, "rake")} RAILS_ENV=#{env} #{rake_cmd}"
+	cmd += "['#{rake_args.join("','")}']" unless rake_args.empty?
+	run cmd
+	set :rakefile, nil if exists?(:rakefile)
+end
+
